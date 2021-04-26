a721879f77d3cabefeaad26c0cbaf4394ec2192b
kosh-cyber <81568292+kosh-cyber@users.noreply.github.com>
2021-04-26 17:43:35 +0800

    add file

--- docker/rsa.py
+++ docker/rsa.py
@@ -2,12 +2,23 @@
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from base64 import b64decode
-from flask import Flask
+from base64 import b64encode
+from flask import Flask,send_from_directory
 from flask import request
+import os
 import time
 
 KEY = "privkey.pem"
 app = Flask(__name__)
+DOWNLOAD_DIRECTORY = os.getcwd() + "/script/"
+
+@app.route('/file/<path:path>',methods = ['GET'])
+def get_files(path):
+    """Download a file."""
+    try:
+        return send_from_directory(DOWNLOAD_DIRECTORY, path, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
 
 @app.route("/session",methods=['POST'])
 def session():
@@ -18,4 +29,4 @@ def session():
     return session.hex()[382:].upper()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=3000)
+    app.run(host="0.0.0.0",port=8080,debug = True)
